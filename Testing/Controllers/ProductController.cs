b69de218145a7ec3483194a0acdeeeeace6aa31f
alica kavuljakova <drobeek@gmail.com>
2023-06-21 18:06:17 -0700

    insert product added

---
+++ Testing/Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Eventing.Reader;
+using System.Reflection.Metadata.Ecma335;
+using Testing.Models;
+
+namespace Testing.Controllers
+{
+    public class ProductController : Controller
+    {
+       
+        private readonly IProductRepository repo;
+
+        public ProductController(IProductRepository repo)
+        {
+            this.repo = repo;
+        }
+
+
+        public IActionResult Index()
+        {
+            var products = repo.GetAllProducts();
+            return View(products);
+        }
+        public IActionResult ViewProduct(int id) 
+        {
+            var product= repo.GetProduct(id);
+            return View(product);
+        }
+        public IActionResult UpdateProduct(int id)
+        {
+            var product= repo.GetProduct(id);
+            if (product == null)
+            {
+
+                return View("ProductNotFound");
+            }
+            return View(product);
+        }
+        public IActionResult UpdateProductToDatabase(Product product)
+        { 
+            repo.UpdateProduct(product);
+        return RedirectToAction("ViewProduct", new {id=product.ProductID});
+        }
+        public IActionResult InsertProduct()
+        {
+            var product = repo.AssignCategory();
+            return View(product);
+        }
+        public IActionResult InsertProductToDatabase(Product productToInsert) 
+        {
+            repo.InsertProduct(productToInsert);
+            return RedirectToAction("Index");
+        
+        }
+       
+    }
+}
---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using Testing.Models;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public Product AssignCategory()
+        {
+           var categoryList=GetCategories();
+            Product product = new Product();
+            product.Categories=categoryList;
+            return product;
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _conn.Query<Category>("Select * from categories");
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("Select * from products");
+        }
+
+        public Product GetProduct(int id)
+        {
+           return _conn.QuerySingle<Product>("Select * from products where ProductID=@id",new {id=id});
+        }
+
+        public void InsertProduct(Product productToInsert)
+        {
+            _conn.Execute("Insert into products (name,price,categoryID) values (@name,@price,@categoryid)",
+            new { name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
+        }
+
+        public void UpdateProduct(Product product)
+        {
+             _conn.Execute("Update products set Name=@name,Price=@price,CategoryID=@categoryId where ProductID=@id", new { name = product.Name, price = product.Price, categoryid = product.CategoryID , id=product.ProductID});
+        }
+    }
+}
